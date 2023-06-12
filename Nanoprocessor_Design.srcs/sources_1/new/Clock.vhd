5ab1a8773e7fe4202acc925739fca73e5121b3a5
Chathura De Silva <ac.chathura@gmail.com>
2023-06-12 16:23:20 +0530

    Checked using a Basys3 Board!

--- Nanoprocessor_Design.srcs/sources_1/new/Clock.vhd
+++ Nanoprocessor_Design.srcs/sources_1/new/Clock.vhd
@@ -45,7 +45,7 @@ begin
     process (Clk_in) begin
         if(rising_edge(Clk_in)) then
             count <= count+1;
-            if(count = 2)then   -- Counting frequency scaler(Reduced to 5 to simulation purposes.IF not 5M.)
+            if(count = 50000000)then   -- Counting frequency scaler(Reduced to 5 to simulation purposes.IF not 5M.)
                 clk_status <= not clk_status;
                 Clk_out <= clk_status;                
                 count<=1;
